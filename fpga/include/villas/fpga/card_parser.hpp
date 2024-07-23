92b4ba98da5d350c24ef6cdd9acbc68684c85973
IgnoreWarnings <pascal.bauer@rwth-aachen.de>
2024-07-23 11:42:44 +0000

    remove devices parsing
    Signed-off-by: IgnoreWarnings <pascal.bauer@rwth-aachen.de>

--- fpga/include/villas/fpga/card_parser.hpp
+++ fpga/include/villas/fpga/card_parser.hpp
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <villas/exceptions.hpp>
-#include <villas/log.hpp>
 #include <jansson.h>
 #include <spdlog/logger.h>
+#include <villas/exceptions.hpp>
+#include <villas/log.hpp>
 
 class CardParser {
 public:
@@ -16,27 +16,15 @@ public:
   int do_reset = 0;
   int affinity = 0;
   int polling = 0;
-  json_t *devices = nullptr;
-  std::vector<std::string> device_names;
 
   CardParser(json_t *json_card) : logger(villas::logging.get("CardParser")) {
     json_error_t err;
     int ret = json_unpack_ex(
-        json_card, &err, 0,
-        "{ s: o, s?: i, s?: b, s?: s, s?: s, s?: b, s?: o, s?: o }", "ips",
-        &json_ips, "affinity", &affinity, "do_reset", &do_reset, "slot",
-        &pci_slot, "id", &pci_id, "polling", &polling, "paths", &json_paths,
-        "devices", &devices);
+        json_card, &err, 0, "{ s: o, s?: i, s?: b, s?: s, s?: s, s?: b, s?: o}",
+        "ips", &json_ips, "affinity", &affinity, "do_reset", &do_reset, "slot",
+        &pci_slot, "id", &pci_id, "polling", &polling, "paths", &json_paths);
 
     if (ret != 0)
       throw villas::ConfigError(json_card, err, "", "Failed to parse card");
-
-    // Devices array parsing
-    size_t index;
-    json_t *value;
-    json_array_foreach(devices, index, value) {
-      auto str = json_string_value(value);
-      this->device_names.push_back(str);
-    }
   }
 };
