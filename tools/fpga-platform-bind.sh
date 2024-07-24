605276c4f6e80fa56abec7559fa7400adcef3bf5
IgnoreWarnings <pascal.bauer@rwth-aachen.de>
2024-07-24 09:49:14 +0000

    add platform device bind script
    Signed-off-by: IgnoreWarnings <pascal.bauer@rwth-aachen.de>

--- tools/fpga-platform-bind.sh
+++ tools/fpga-platform-bind.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Bind ips to vfio#
-# Pass the device tree names e.g. "a0000000.dma a291229.switch ..." as comandline arguments
+# Bind ips to vfio
+# Pass the device tree names e.g. "a0000000.dma a291229.switch ..." (space seperated) as comandline arguments.
 #
 # Author: Pascal Bauer <pascal.bauer@rwth-aachen.de>
 # SPDX-FileCopyrightText: 2023-2024 Pascal Bauer <pascal.bauer@rwth-aachen.de>
@@ -10,7 +10,7 @@
 modprobe vfio_platform reset_required=0
 
 i=1;
-for device in "$@" 
+for device in "$@"
 do
     echo "Device - $device";
 
